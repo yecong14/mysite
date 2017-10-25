e6b2a330e8eb5a90e51ab1021f88f9be66720f63
Clarence <yecong2302230@163.com>
2017-10-26 01:41:37 +0800

    增加了详情页，并链接到主页的文章目录中。

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
         url(r'^$',views.blog_title,name='blog_title'),
+        url(r'^blogarticles/(?P<article_id>\d+)/$',views.blog_articles,name='blog_articles'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,6 @@ def blog_title(request):
     blogs = BlogArticles.objects.all()
     return render(request,'blog/titles.html',{'blogs':blogs})
 
+def blog_articles(request,article_id):
+    blog = BlogArticles.objects.get(id=article_id)
+    return render(request,'blog/articles.html',{'blog':blog})
