1002ea66f6d48560389d5d7aa54ce4d8d899eeab
Clarence <yecong2302230@163.com>
2017-11-03 01:12:51 +0800

    some new change

--- account/urls.py
+++ account/urls.py
@@ -8,7 +8,12 @@ urlpatterns = [
 		url(r'^login/$',auth_views.login,{'template_name':'account/login.html'},name='user_login'),
 		url(r'^logout/$',auth_views.logout,{'template_name':'account/logout.html'},name='user_logout'),
 		url(r'^register/$',views.register,name='user_register'),
-		url(r'^password-change/$',auth_views.password_change,{'template_name':'account/password_change.html'},name='password_change'),
-		url(r'^password-change-done/$',auth_views.password_change_done,{'template_name':'account/password_change_done.html'},name='password_change_done'),
+                url(r'^password-change/$',auth_views.password_change,
+                    {'post_change_redirect':'/account/password-change-done',
+                    'template_name':'account/password_change.html'},
+                    name='password_change'),
+		url(r'^password-change-done/$',auth_views.password_change_done,
+                    {'template_name':'account/password_change_done.html'},
+                    name='password_change_done'),
     ]
 
